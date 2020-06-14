7d6c0d413b38460d5edeaeb02710af5c270344dd
Kai Matsuda <kai.matsuda@disneystreaming.com>
2020-06-14 14:02:59 -0400

    .scalafmt.conf

--- server/src/main/scala/hacker/cup/poc/server/Main.scala
+++ server/src/main/scala/hacker/cup/poc/server/Main.scala
@@ -3,6 +3,7 @@ package hacker.cup.poc.server
 import cats.effect.{ExitCode, IO, IOApp}
 
 object Main extends IOApp {
+
   def run(args: List[String]): IO[ExitCode] =
     for {
       _ <- IO(println("hello world"))
